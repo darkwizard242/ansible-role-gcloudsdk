ef9afa4adf4831c95afae9de987868c97e718279
darkwizard242 <am900820@gmail.com>
2020-02-03 18:32:09 -0500

    fix for flake8 E501

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,13 +7,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_gcloudsdk_repofile_exists(host):
-    assert host.file('/etc/apt/sources.list.d/google-cloud-sdk.list').exists or \
-      host.file('/etc/yum.repos.d/google-cloud-sdk.repo').exists
+    assert host.file('/etc/apt/sources.list.d/google-cloud-sdk.list').exists \
+      or host.file('/etc/yum.repos.d/google-cloud-sdk.repo').exists
 
 
 def test_gcloudsdk_repofile_isfile(host):
-    assert host.file('/etc/apt/sources.list.d/google-cloud-sdk.list').is_file or \
-      host.file('/etc/yum.repos.d/google-cloud-sdk.repo').is_file
+    assert host.file('/etc/apt/sources.list.d/google-cloud-sdk.list').is_file \
+      or host.file('/etc/yum.repos.d/google-cloud-sdk.repo').is_file
 
 
 def test_gcloudsdk_package_installed(host):
